6b2b99321c38987e29f34b1736903abb6c57ebc4
gharjare <gharjarechetan@gmail.com>
2022-10-30 14:42:11 +0530

    added node file

---
+++ Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node<T>
+    {
+
+
+
+        public T data;
+        public Node<T> left;
+        public Node<T> right;
+        public Node(T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }      
+    
+}
+
